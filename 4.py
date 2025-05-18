dc23e38f311c591b55f1eaba41fe592c5645c8bb
Letícia Tambelli <leticiatambelli2@gmail.com>
2025-05-18 13:20:51 -0300

    exercicios 4 e 5

---
+++ 4.py
@@ -0,0 +1,7 @@
+pessoa = {'nome': 'Ana', 'idade': 21, 'cidade': 'Itapetininga'}
+
+if 'nome' in pessoa:
+    print('A chave existe no dicionário.')
+else:
+    print('A chave não existe no dicionário.')
+    
